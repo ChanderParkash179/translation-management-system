d25819878e87905219e157e5889ba10066a4e45e
Chander Parkash <chanderparkash179@gmail.com>
2025-03-01 11:23:19 +0500

    init commit

---
+++ src/test/java/com/tms/app/TranslationManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tms.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslationManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
